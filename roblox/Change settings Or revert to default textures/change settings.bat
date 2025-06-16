aec6ed6ec47e6260f8f3c864a1f32cb8032314ff
KickfnGIT <github.canopy086@passmail.com>
2025-06-16 11:39:00 -0400

    Delete roblox/Change settings Or revert to default textures directory

--- roblox/Change settings Or revert to default textures/change settings.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "C:\Users\steve\Documents\roblox\Change settings Or revert to default textures\powershell script (do not touch)\Change settings.ps1"
-exit 
