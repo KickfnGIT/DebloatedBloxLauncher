e7244fef2277d907f668f2c428c489ce6bea7d80
KickfnGIT <github.canopy086@passmail.com>
2025-06-16 11:08:57 -0400

    Add files via upload

---
+++ roblox/Change settings Or revert to default textures/change settings.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "C:\Users\steve\Documents\roblox\Change settings Or revert to default textures\powershell script (do not touch)\Change settings.ps1"
+exit 
